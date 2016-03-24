3d6455941f31816b8c83c3d0a6482c2acc09b6f1
phichalai <persian-cat@hotmail.com>
2016-03-24 15:43:59 +0700

    delete All finish

--- app/src/main/java/rmutp/wongdoywang/phichalai/toursouthchilchil/MainActivity.java
+++ app/src/main/java/rmutp/wongdoywang/phichalai/toursouthchilchil/MainActivity.java
@@ -1,6 +1,7 @@
 package rmutp.wongdoywang.phichalai.toursouthchilchil;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,12 +21,23 @@ public class MainActivity extends AppCompatActivity {
         myManage = new MyManage(this);
 
         //test add value
-        testaddValue();
+        //testaddValue();
+
+        //Delete All data
+        deleteAllData();
+
 
 
 
 
     }//Main Method
+
+    private void deleteAllData() {
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        sqLiteDatabase.delete(MyManage.user_table, null, null);
+        sqLiteDatabase.delete(MyManage.tour_table, null, null);
+    }
 
     private void testaddValue() {
         myManage.addUser("user", "pass", "name", "email");
