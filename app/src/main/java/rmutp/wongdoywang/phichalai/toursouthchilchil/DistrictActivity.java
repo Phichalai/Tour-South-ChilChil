67736390d865329bbccb1a1f768a7c0921da1385
phichalai <persian-cat@hotmail.com>
2016-04-08 11:02:09 +0700

    show district Finish

--- app/src/main/java/rmutp/wongdoywang/phichalai/toursouthchilchil/DistrictActivity.java
+++ app/src/main/java/rmutp/wongdoywang/phichalai/toursouthchilchil/DistrictActivity.java
@@ -1,8 +1,14 @@
 package rmutp.wongdoywang.phichalai.toursouthchilchil;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.io.InputStream;
 
 public class DistrictActivity extends AppCompatActivity {
 
@@ -12,13 +18,33 @@ public class DistrictActivity extends AppCompatActivity {
         setContentView(R.layout.activity_district);
 
         //Receive from Intent
-        String[] districtStrings = getIntent().getStringArrayExtra("District");
+        final String[] districtStrings = getIntent().getStringArrayExtra("District");
         String stringProvince = getIntent().getStringExtra("Province");
 
         //show Province
         TextView provinceTextView = (TextView) findViewById(R.id.textView7);
         provinceTextView.setText("อำเภอในจังหวัด " + stringProvince);
 
+        //show ListView
+        MyAdpter myAdpter = new MyAdpter(this, districtStrings);
+        ListView listView = (ListView) findViewById(R.id.listView2);
+        listView.setAdapter(myAdpter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(DistrictActivity.this, TourActivity.class);
+                intent.putExtra("District", districtStrings[i]);
+                startActivity(intent);
+            }//onitem
+        });
+
 
     }//Main Method
+
+
+    public void clickBackDistrict(View view) {
+        finish();
+    }
+
 }//Main Class
