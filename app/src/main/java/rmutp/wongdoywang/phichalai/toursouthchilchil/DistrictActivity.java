14fa54757e52193486582f44f4fa93e28728b211
phichalai <persian-cat@hotmail.com>
2016-04-08 10:41:12 +0700

    show Province Finish

--- app/src/main/java/rmutp/wongdoywang/phichalai/toursouthchilchil/DistrictActivity.java
+++ app/src/main/java/rmutp/wongdoywang/phichalai/toursouthchilchil/DistrictActivity.java
@@ -2,6 +2,7 @@ package rmutp.wongdoywang.phichalai.toursouthchilchil;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DistrictActivity extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class DistrictActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_district);
-    }
-}
+
+        //Receive from Intent
+        String[] districtStrings = getIntent().getStringArrayExtra("District");
+        String stringProvince = getIntent().getStringExtra("Province");
+
+        //show Province
+        TextView provinceTextView = (TextView) findViewById(R.id.textView7);
+        provinceTextView.setText("อำเภอในจังหวัด " + stringProvince);
+
+
+    }//Main Method
+}//Main Class
