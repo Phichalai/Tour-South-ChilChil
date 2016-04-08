9bb2a77d24860e941d4ca942e9339a069efd6bf3
phichalai <persian-cat@hotmail.com>
2016-04-08 11:46:31 +0700

    Tour Finish

--- app/src/main/java/rmutp/wongdoywang/phichalai/toursouthchilchil/TourActivity.java
+++ app/src/main/java/rmutp/wongdoywang/phichalai/toursouthchilchil/TourActivity.java
@@ -1,7 +1,10 @@
 package rmutp.wongdoywang.phichalai.toursouthchilchil;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -12,6 +15,7 @@ public class TourActivity extends AppCompatActivity {
     private String districtString;
     private TextView districtTextView;
     private ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,16 +23,46 @@ public class TourActivity extends AppCompatActivity {
 
         //Bind Widget
         districtTextView = (TextView) findViewById(R.id.textView8);
+        listView = (ListView) findViewById(R.id.listView3);
 
         //Receive Value
         districtString = getIntent().getStringExtra("District");
         districtTextView.setText(districtString);
 
+        //Show List Tour
+        showListTour();
 
-    }//Main Method
+
+
+    }   // Main Method
+
+    private void showListTour() {
+
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE, null);
+        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM tourTABLE WHERE District = " + "'" + districtString + "'", null);
+        cursor.moveToFirst();
+
+        String[] tourStrings = new String[cursor.getCount()];
+
+        for (int i = 0; i < cursor.getCount(); i++) {
+
+            tourStrings[i] = cursor.getString(cursor.getColumnIndex(MyManage.column_Name));
+
+            cursor.moveToNext();
+        }   //for
+        cursor.close();
+
+        Log.d("8April", "tourString Lang ==>> " + cursor.getCount());
+
+        MyAdpter myAdapter = new MyAdpter(this, tourStrings);
+        listView.setAdapter(myAdapter);
+
+
+    }   // showListTour
 
     public void clickBackTour(View view) {
         finish();
     }
 
-}//Main Class
+}   // Main Class
