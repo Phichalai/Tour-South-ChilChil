8a2b43d86133e6ae23270d6053b66fd83714e6b2
phichalai <persian-cat@hotmail.com>
2016-05-27 17:39:36 +0700

    Finish 27 05 2559 22  22

---
+++ app/src/main/java/rmutp/wongdoywang/phichalai/toursouthchilchil/RangeActvity.java
@@ -0,0 +1,10 @@
+package rmutp.wongdoywang.phichalai.toursouthchilchil;
+
+/**
+ * Created by Phichalai on 27/5/2559.
+ */
+
+
+public class RangeActvity {
+}
+
--- app/src/main/java/rmutp/wongdoywang/phichalai/toursouthchilchil/admin.java
+++
@@ -1,7 +0,0 @@
-package rmutp.wongdoywang.phichalai.toursouthchilchil;
-
-/**
- * Created by Phichalai on 17/5/2559.
- */
-public class admin {
-}
